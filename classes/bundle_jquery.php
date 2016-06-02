1d1d726bcd38b4efc963d757ae64d4a94a6a04eb
Matt Bruton <matt.bruton@gmail.com>
2016-06-02 17:13:46 +0100

    Updates

--- classes/bundle_jquery.php
+++ classes/bundle_jquery.php
@@ -7,17 +7,18 @@ namespace jquery{
     
     class bundle_jquery extends \adapt\bundle{
         
-        public function __construct(){
-            print "Contructing jquery";
-            parent::__construct('jquery');
+        public function __construct($data){
+            parent::__construct('jquery', $data);
         }
         
-        public function boot($boot_dependencies = true){
-            if (!$this->has_booted){
-                parent::boot($boot_dependencies);
+        public function boot(){
+            if (parent::boot()){
                 
                 $this->dom->head->add(new html_script(array('type' => 'text/javascript', 'src' => "/adapt/jquery/jquery-{$this->version}/static/js/jquery.min.js")));
+                return true;
             }
+            
+            return false;
         }
         
     }
